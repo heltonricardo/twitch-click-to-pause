9f81975392c87e781c5f2163d2244b5797125590
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2023-05-24 14:56:13 -0300

    fix: video pause bug on control bar click

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const PLAYER_SELECTOR = "div[data-a-target='video-player']";
+const CONTROLS_SELECTOR = "div[data-a-target='player-controls']";
 const PAUSE_BTN_SELECTOR = "button[data-a-target='player-play-pause-button']";
 
-function addPauseEventListener(player, pauseButton) {
+function addPauseEventListener(player, controls, pauseButton) {
   const pauseVideo = () => pauseButton.click();
-  player.addEventListener("click", pauseVideo);
+
+  player.addEventListener("click", (event) => {
+    controls.contains(event.target) || pauseVideo();
+  });
 }
 
 function observePlayerAndButton() {
   const player = document.querySelector(PLAYER_SELECTOR);
+  const controls = document.querySelector(CONTROLS_SELECTOR);
   const pauseButton = document.querySelector(PAUSE_BTN_SELECTOR);
 
-  if (player && pauseButton) {
+  if (player && controls && pauseButton) {
     observer.disconnect();
-    addPauseEventListener(player, pauseButton);
+    addPauseEventListener(player, controls, pauseButton);
   }
 }
 
